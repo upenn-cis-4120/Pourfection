39904eb8b2ea09c31fa6b34cfc6a3c22810948b7
cdeteindre <cedric.deteindre@inf.ethz.ch>
2024-11-07 15:08:57 -0500

    allpagesadded

---
+++ src/screens/Screen8/Screen8.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./style.css";
+
+export const Iphone = () => {
+  return (
+    <div className="iphone">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+
+          <div className="text-wrapper">Initialization</div>
+        </div>
+
+        <div className="overlap-group">
+          <div className="rectangle-2" />
+
+          <div className="text-wrapper-2">Calibration</div>
+        </div>
+
+        <div className="div-wrapper">
+          <div className="text-wrapper-3">Suggested Upgrades</div>
+        </div>
+
+        <div className="overlap-2">
+          <div className="rectangle-3" />
+
+          <div className="text-wrapper-4">Drink Selection</div>
+        </div>
+
+        <div className="overlap-3">
+          <div className="text-wrapper-4">Steamer Guide</div>
+        </div>
+
+        <div className="overlap-4">
+          <div className="text-wrapper-5">Cleaning Guide</div>
+        </div>
+
+        <div className="overlap-5">
+          <div className="text-wrapper-5">User Manuals</div>
+        </div>
+
+        <div className="overlap-6">
+          <div className="text-wrapper-6">Main Menu</div>
+
+          <div className="frame">
+            <div className="group">
+              <div className="vector-wrapper">
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://c.animaapp.com/3fYgMAq0/img/vector.svg"
+                />
+              </div>
+
+              <div className="img-wrapper">
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://c.animaapp.com/3fYgMAq0/img/vector-1.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
